767d34ab3e0ea094762e2257181328f2f4234ab9
Delroy Brown <delroybrown.db@gmail.com>
2024-07-25 15:47:22 +0100

    Updated SECRET_KEY and DEBUG variable assignments in settings.py

--- DelroyBrownPortfolio_base/settings.py
+++ DelroyBrownPortfolio_base/settings.py
@@ -9,9 +9,9 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
-DEBUG = os.getenv("DEBUG")
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ["delroy-brown-portfolio-4615d064d1ba.herokuapp.com", "localhost"]
 
