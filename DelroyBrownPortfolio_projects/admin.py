3d4f4b5f3ff8568dc5d91ed820a9fccb38a066ed
Delroy Brown <delroybrown.db@gmail.com>
2024-08-18 23:12:05 +0100

    Removed empty migration files, updated admin.py to register models with custom admin interface, added ProgLanguageUsed model and updated ProjectDetails model to include ManyToManyField, and modified project_detail.html template to display programming language information

--- DelroyBrownPortfolio_projects/admin.py
+++ DelroyBrownPortfolio_projects/admin.py
@@ -1,4 +1,13 @@
+# DelroyBrownPortfolio_projects/admin.py
 from django.contrib import admin
-from .models import ProjectDetails
+from .models import ProjectDetails, ProgLanguageUsed
 
-admin.site.register(ProjectDetails)
+
+@admin.register(ProjectDetails)
+class ProjectDetailsAdmin(admin.ModelAdmin):
+    filter_horizontal = (
+        "prog_languages_used",
+    )  # This makes the ManyToManyField easier to manage in the admin
+
+
+admin.site.register(ProgLanguageUsed)
--- DelroyBrownPortfolio_projects/models.py
+++ DelroyBrownPortfolio_projects/models.py
@@ -2,6 +2,13 @@
 from django.db import models
 
 
+class ProgLanguageUsed(models.Model):
+    language_name = models.CharField(max_length=50, blank=False, null=False, default="")
+
+    def __str__(self):
+        return self.language_name
+
+
 class ProjectDetails(models.Model):
     project_name = models.CharField(max_length=25, blank=False, null=False, default="")
     project_description = models.TextField(blank=False, null=False, default="")
@@ -9,6 +16,8 @@ class ProjectDetails(models.Model):
     image_1 = models.ImageField(upload_to="project-images", blank=False, null=False)
     image_2 = models.ImageField(upload_to="project-images", blank=True, null=True)
     image_3 = models.ImageField(upload_to="project-images", blank=True, null=True)
+    prog_languages_used = models.ManyToManyField(ProgLanguageUsed)
+    prog_language_reason = models.TextField(blank=False, null=False, default="")
 
     def __str__(self):
         return self.project_name
